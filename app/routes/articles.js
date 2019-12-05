7a5e7f2580c102e75858a3589edb6d79c2a701c3
Mesfer <mesfer.vip@gmail.com>
2019-12-05 14:29:36 +0300

    create article route

---
+++ app/routes/articles.js
@@ -0,0 +1,63 @@
+//Requir necessary NPM Packages 
+const express = require('express');
+
+// Require mongoose Model for article
+const Article = require('../models/article')
+
+// instantiate a router (mini app that only handles routes)
+const router = express.Router();
+
+/**
+ * Action .......INDEX
+ * Method .......Get
+ * URI:   .......(/api/articles)
+ * Description: Get all Articles
+ */
+
+router.get('/api/articles', (req, res)=>{
+    Article.find().then((article)=>{
+        // return all articles as an array
+        res.status(200).json({message: 'Get all Articles'});
+
+    })
+    //catch any errors that might occur
+    .catch((error)=>{
+        res.status(500).json({error: error});
+    })
+});
+
+/**
+ * Action .......SHOW
+ * Method .......Get
+ * URI:   .......(/api/articles/id)
+ * Description: Get an Article by Article ID
+ */
+
+
+
+ /**
+ * Action .......CREATE
+ * Method .......POST
+ * URI:   .......(/api/articles)
+ * Description: Create a New Article
+ */
+
+
+
+ /**
+ * Action .......UPDATE
+ * Method .......PATCH         ...Patch for mutiple id and put for one 
+ * URI:   .......(/api/articles/id)
+ * Description: Update an Article by Article ID
+ */
+
+
+
+/**
+ * Action .......DESTROY
+ * Method .......DELETE          
+ * URI:   .......(/api/articles/id)
+ * Description: Delete an Article by Article ID
+ */
+//export the router so we can use it in the server.js file
+module.exports = router;
