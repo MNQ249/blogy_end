51912f66411363322d0e3d96fbccff709b21de96
Mesfer <mesfer.vip@gmail.com>
2019-12-05 14:26:10 +0300

    add config

--- server.js
+++ server.js
@@ -6,6 +6,17 @@ const mongoose = require('mongoose');
 
 // requir route files
 const indexRouter = require('./app/routes/index');
+const articlesRouter = require('./app/routes/articles');
+
+
+// Require DB Configuration File
+const db = require('./config/db');
+
+// Establish database connection
+mongoose.connect(db, {useNewUrlParser: true});
+mongoose.connection.once('open', ()=>{
+    console.log('Connected to MongoDB');
+})
 
 
 // instantiate Express Application Object
@@ -18,6 +29,7 @@ const port = process.env.PORT || 5000; //comes from nodejs if exist run else run
 
 /** Routes ***/
 app.use(indexRouter);
+app.use(articlesRouter);
 
 // start the serever to listen for requests on a give port
 app.listen(port, ()=>{
